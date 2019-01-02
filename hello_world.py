9b954190a3200226b905317f160665566133b95b
oreganc2 <oreganc2@tcd.ie>
2019-01-02 16:43:16 +0000

    adding hello_world

---
+++ hello_world.py
@@ -0,0 +1,16 @@
+
+import requests
+
+
+
+def call_http_get():
+  r = requests.get('https://api.github.com/events')
+  print(r.json())
+
+
+def main():
+  print "hello world"
+  call_http_get()
+
+if __name__ == '__main__':
+  main()
